283f03b32318b55e8d4f319450ae4e97b61afcd8
Young Lee <ylee8575@gmail.com>
2018-01-22 00:09:12 -0800

    Installed browserify and babelify json package and added bundle and bundle:watch script instructions.

---
+++ display.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Selector } from './selector';
+import { Ruins } from './ruins';
+
+class Display extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'Ruins' };
+    this.changeName = this.changeName.bind(this)
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Selector onChange={this.changeName} />
+        <Ruins name={this.state.name} />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Display />,
+  document.getElementById('app')
+)
---
+++ selector.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export class Selector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.value;
+    this.props.onChange(name);
+  }
+
+  render() {
+    return (
+      <div>
+        <select
+          id="locations"
+          onChange={this.handleChange}>
+
+          <option value="Ruins">Ruins</option>
+          <option value="Warrens">Warrens</option>
+          <option value="Weald">Weald</option>
+          <option value="Cove">Cove</option>
+          <option value="Courtyard">Courtyard</option>
+          </select>
+      </div>
+    );
+  }
+}
